0aa0d573759c979cea2f9311e02a06126985fd20
dionisioerikayazmin1c@gmail.com <61323708+ErikaYazminDionisio@users.noreply.github.com>
2022-03-29 23:24:36 -0600

    firebase
    Actualización BD

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,20 @@
+//Función autoinbocada para proteger
+//variables
 (function(){
+    //Obtiene el contenido de nav_menu
     const  openButton = document.querySelector('.nav_menu');
+    //Obtiene el icono menu
     const menu = document.querySelector('.nav_link');
+    //Obtiene el ícono cerrar
     const closeMenu = document.querySelector('.nav_close');
 
+    //Al oprimir el ícono menu, ejecutara la función de la visualización
+    //del contenido donde se encuentran los links de acceso
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav_link--show');
     });
+    //Al oprimir el ícono cerrar, ejecutara la función para ocultar
+    //el contenido donde se encuentran los links de acceso
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav_link--show');
     });
